eb01dfda20013423387c16f05060a9ec04ab8bad
BHAKTISHAH <shahbhakti21@gmail.com>
2023-11-30 23:36:03 -0600

    pls

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,15 +1,45 @@
 module Day01
     ( part1, part2
     ) where
+import Data.Char
+import Data.List.Split
 
-import Data.List.Split ( splitOn )
-import Data.List ( sort )
-
-total_list :: String -> [Int]
-total_list = (fmap (foldl (+) 0 . fmap read)) . (splitOn [""]) . lines 
+validList = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 part1 :: String -> Int
-part1 = maximum . total_list
+part1 str = sum $ map (\ x -> read $ (head (filter isDigit x)) : [(last (filter isDigit x))]) (lines str)
 
 part2 :: String -> Int
-part2 = sum . (take 3) . reverse . sort . total_list
+part2 str = sum $ map (\x -> (10 * part2Helper x) + (part2Helper2 (reverse x))) $ lines str
+
+part2Helper :: String -> Int
+part2Helper chars@(x:xs) = if isDigit x then (digitToInt x) else
+    case (take 3 chars) of 
+    "one" -> 1
+    "two" -> 2
+    "six" -> 6
+    _ -> case (take 4 chars) of 
+        "four" -> 4
+        "five" -> 5
+        "nine" -> 9
+        _ ->  case (take 5 chars) of 
+           "three" -> 3
+           "seven" -> 7
+           "eight" -> 8
+           _ -> part2Helper xs
+
+part2Helper2 :: String -> Int
+part2Helper2 chars@(x:xs) = if isDigit x then (digitToInt x) else
+    case (take 3 chars) of 
+   "eno" -> 1
+   "owt" -> 2
+   "xis" -> 6
+   _ -> case (take 4 chars) of 
+       "ruof" -> 4
+       "evif" -> 5
+       "enin" -> 9
+       _ ->  case (take 5 chars) of 
+           "eerht" -> 3
+           "neves" -> 7
+           "thgie" -> 8
+           _ -> part2Helper2 xs
